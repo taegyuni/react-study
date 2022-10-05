ddfece7969c0b364f20c25f58629e67fe0f1303f
taegyuni <taegyun7553@naver.com>
2022-10-05 15:58:22 +0900

    .gitignore 생성

--- src/Container.js
+++ src/Container.js
@@ -1,9 +1,8 @@
-const Container = ({children})=>{
-    return (
-        <div style={{margin:20,padding:20,border:"1px solid"}}>
-            {children}
-        </div>
-    )
-    
-}
-export default Container
+const Container = ({ children }) => {
+  return (
+    <div style={{ margin: 20, padding: 20, border: "1px solid" }}>
+      {children}
+    </div>
+  );
+};
+export default Container;
